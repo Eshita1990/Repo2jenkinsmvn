7d4a23e78be789c8b3dcb6ad723263c419b609e1
Eshi <eshi@gmail.com>
2024-11-09 10:39:44 -0600

    Added maven

---
+++ MavenOne/src/main/java/Mvn/One/App.java
@@ -0,0 +1,10 @@
+package Mvn.One;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ MavenOne/src/main/java/Mvn/One/mvn1.java
@@ -0,0 +1,10 @@
+package Mvn.One;
+
+public class mvn1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello world from maven");
+	}
+
+}
